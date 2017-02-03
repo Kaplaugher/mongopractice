1ecc3e976c182a5de24c836326fe549edf002258
Kaplaugher <kaplaugher94@gmail.com>
2017-02-03 16:25:12 -0500

    init

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var mongoConnectUrl = "mongodb://localhost:27017/dcclass"
+var db;
+
+mongoClient.connect(mongoConnectUrl,(error,database)=>{
+  db = database;
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  db.collection('students').find({}).toArray((error,studentResults)=>{
+    console.log(studentResults);
+    res.render('index', { students: studentResults });
+  });
+
+});
+
+module.exports = router;
